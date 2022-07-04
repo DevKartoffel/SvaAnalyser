7716aecc01dbaa62a3bfc2f21c0445e19eb74da7
Janis Voß <janis.voss@haw-hamburg.de>
2022-07-04 21:24:55 +0200

    jonas + christian =<3

--- run.py
+++ run.py
@@ -1,14 +1,7 @@
-from analyser.sva_requests import SvaRequests
+from analyser.analyser import Analyser
 import os
 
-mail = os.environ.get('EMAIL')
-pw = os.environ.get('PW')
-base_url = os.environ.get('BASE_URL')
-
-sva_requ = SvaRequests(mail, pw,base_url)
-
-token_resp = sva_requ.get_token()
-flyovers = sva_requ.get_fylovers('')
-nutmegs =sva_requ.get_nutmegs('')
+an = Analyser()
+an.run()
 
 print('end')
